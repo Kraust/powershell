c49998899e799dacf11d0865e53fda9205eb3c19
Kraust <secretdragoon@gmail.com>
2025-07-24 11:30:53 +0000

    Formatting.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,16 +3,22 @@ Clear-Host
 $ENV:GIT_CONFIG_GLOBAL="$HOME/.config/gitconfig/gitconfig"
 $ENV:STARSHIP_CONFIG = "$HOME/.config/starship/starship.toml"
 
-$separator = if ($IsWindows) { ";" } else { ":" }
+$separator = if ($IsWindows)
+{ ";" 
+} else
+{ ":" 
+}
 $ENV:PATH += $ENV:PATH + $separator + "$HOME/.cargo/bin"
 
-function Load-Module {
+function Load-Module
+{
 	param(
 		[Parameter(Mandatory=$true)]
 		[string]$Name
 	)
-	if (-not (Get-Module -ListAvailable -Name $Name)) {
-			Install-Module -Name $Name -Scope CurrentUser -Force
+	if (-not (Get-Module -ListAvailable -Name $Name))
+	{
+		Install-Module -Name $Name -Scope CurrentUser -Force
 	}
 }
 
@@ -22,7 +28,7 @@ Load-Module nvm
 
 
 #PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+# Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
 # ps-nvm
@@ -33,8 +39,10 @@ Set-NodeVersion 21
 Invoke-Expression (&starship init powershell)
 
 # Load Work Env:
-if (Test-Path $HOME/scripts/env.ps1) {
+if (Test-Path $HOME/scripts/env.ps1)
+{
 	Import-Module $HOME/scripts/env.ps1
-} else {
+} else
+{
 	Write-Host "env.ps1 not loaded"
 }
